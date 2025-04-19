4a655f2d08929eb0e6423e914b2af17548927b19
Sunly <sonlychao169@gmail.com>
2025-04-20 05:59:09 +0700

    create search_bar

---
+++ lib/ui/screen/time_tracker_screen/widgets/search_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  final Function(String)? onChanged;
+  final String? hintText;
+
+  const SearchBar({
+    super.key,
+    this.onChanged,
+    this.hintText = 'Search BIB Number',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: TextField(
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey[400], fontSize: 16),
+          prefixIcon: Icon(Icons.search, color: Colors.grey[400]),
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
